aabe8ad98a461217133b12760f11fa9edc72dfce
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2022-01-07 09:39:39 +0100

    fini

--- index.php
+++ index.php
@@ -6,10 +6,12 @@ echo "Un fichier php va être inclu à la suite de ce texte ...<br><br>";
 
 // inclure fichier texte.php ci dessous
 
+include "texte.php";
+
 
 // Deuxieme Ligne ( inclure fichiers vars.php ci dessous )
 
-
+include_once "vars.php";
 
 echo "J'aime également les " . $maVar;
 
